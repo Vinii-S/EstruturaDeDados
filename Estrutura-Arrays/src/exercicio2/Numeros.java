0508e2bd6080f106362afb3ac7e1c8b0fc1b5ec6
Vinícius Santos <viny070805@gmail.com>
2024-03-05 12:20:59 -0300

    Update Numeros.java

--- Estrutura-Arrays/src/exercicio2/Numeros.java
+++ Estrutura-Arrays/src/exercicio2/Numeros.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 public class Numeros {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int numeros[] = new int[5];
-        System.out.println("Informe cinco numeros: ");
+        int numeros[] = new int[10];
+        System.out.println("Informe dez numeros: ");
         for(int i = 0; i<numeros.length;i++){
             numeros[i] = input.nextInt();
         }
