8019b4a13552522a42e4d2132d64801d74a2b192
Vinícius Santos <viny070805@gmail.com>
2024-03-06 10:01:07 -0300

    Exercicio Banco

--- Estrutura-Arrays/src/exercicio3/RegraNegocioEmpresa.java
+++ Estrutura-Arrays/src/exercicio3/RegraNegocioEmpresa.java
@@ -1,7 +1,7 @@
 package exercicio3;
 
 public class RegraNegocioEmpresa {
-   private Empresa[] e = new Empresa[2];
+   private Empresa[] e = new Empresa[4];
 
     public void cadastrarEmpresa(Empresa emp){
         for(int i = 0; i<e.length;i++){
