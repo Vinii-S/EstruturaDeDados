b1388e515ceba1b1510d8a91b3f0e9d548cb4922
Vinícius Santos <viny070805@gmail.com>
2024-07-22 23:02:46 -0300

    Utilização de métodos

--- Exercicio-Biblioteca/Biblioteca-teste/src/main.java
+++ Exercicio-Biblioteca/Biblioteca-teste/src/main.java
@@ -5,11 +5,14 @@ public class main {
         Album a1 = new Album("IGOR","TYLER, THE CREATOR", 2019);
         Album a2 = new Album("Call me if you get lost", "TYLER, THE CREATOR", 2021);
         Album a3 = new Album("To Pimp a Butterfly", "Kendrick Lamar", 2015);
+        Album a4 = new Album("recess", "bbno$", 2019);
 
-        Cadastros<Album> registro = new Cadastros<Album>();
-        registro.adicionar(a1);
-        registro.adicionar(a2);
-        registro.adicionar(a3);
-        registro.listar();
+        Cadastros<Album> catalogo = new Cadastros<Album>();
+        catalogo.adicionar(a1);
+        catalogo.adicionar(a2);
+        catalogo.adicionar(a3);
+        catalogo.adicionarPorPosicao(a4,1);
+        catalogo.remover(2);
+        catalogo.listar();
     }
 }
