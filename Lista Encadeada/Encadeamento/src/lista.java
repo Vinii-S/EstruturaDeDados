d3344519928615bdcf04c0e0a1f258010efa4178
Vinícius Santos <viny070805@gmail.com>
2024-08-05 22:18:55 -0300

    Lista Encadeada com Adicionar e Remover por posição

--- Lista Encadeada/Encadeamento/src/lista.java
+++ Lista Encadeada/Encadeamento/src/lista.java
@@ -25,6 +25,54 @@ public class lista {
         this.total++;
     }
 
+    public void adicionarPosicao(int posicao, String nome) {
+        if (posicao < 0 || posicao > total) {
+           System.out.println("Posição inválida");
+        }
+    
+        if (posicao == 0) {
+            adicionarcomeco(nome);
+        } else if (posicao == total) {
+            adicionarfim(nome);
+        } else {
+            celula nova = new celula(nome);
+            celula aux = cabeca;
+            for (int i = 0; i < posicao - 1; i++) {
+                aux = aux.proxima;
+            }
+            nova.setProxima(aux.proxima);
+            aux.setProxima(nova);
+            total++;
+        }
+    }
+
+    public void removerPosicao(int posicao) {
+        if (posicao < 0 || posicao >= total) {
+            System.out.println("Posição inválida");
+        }
+    
+        if (posicao == 0) {
+            cabeca = cabeca.proxima;
+            if (total == 1) {
+                cauda = null;
+            }
+        } else if (posicao == total - 1) {
+            celula aux = cabeca;
+            for (int i = 0; i < posicao - 1; i++) {
+                aux = aux.proxima;
+            }
+            aux.setProxima(null);
+            cauda = aux;
+        } else {
+            celula aux = cabeca;
+            for (int i = 0; i < posicao - 1; i++) {
+                aux = aux.proxima;
+            }
+            aux.setProxima(aux.proxima.proxima);
+        }
+        total--;
+    }
+    
     public void imprimir(){
         celula aux = this.cabeca;
         for(int i =0; i<total;i++){
--- Lista Encadeada/Encadeamento/src/main.java
+++ Lista Encadeada/Encadeamento/src/main.java
@@ -5,10 +5,23 @@ public class main {
     lista primeira = new lista();
 
     Scanner input = new Scanner(System.in);
+    
         primeira.adicionarfim(input.nextLine());
         primeira.adicionarfim(input.nextLine());
         primeira.adicionarfim(input.nextLine());
+       
+        primeira.adicionarcomeco(input.nextLine());
+        primeira.adicionarcomeco(input.nextLine());
+        primeira.adicionarcomeco(input.nextLine());
+        
+        primeira.imprimir();
 
+        System.out.println("Digite a posição que deseja adicionar: ");
+        primeira.adicionarPosicao(input.nextInt(), input.nextLine());
+        primeira.imprimir();
+
+        System.out.println("Digite a posição que deseja remover: ");
+        primeira.removerPosicao(input.nextInt());
         primeira.imprimir();
     }
 }
